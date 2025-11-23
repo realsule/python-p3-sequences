b5c995204d25ebe9c066420b3dcb297a065a5d4b
Suleiman <real1sule@gmail.com>
2025-11-23 03:13:32 +0300

    complete python-sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+
+    fib = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+    while len(fib) < length:
+        fib.append(fib[-1] + fib[-2])
+
+    print(fib[:length])
+
+    
